247e23b3125797fc83e0638676c8b4e7df64a0f4
Nathan Whittle <nathan.whittle@colheli.com>
2020-02-21 15:19:47 -0800

    Working Test Demo
    testable on iOS

---
+++ CarouselViewTest/CarouselViewTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CarouselViewTest.ViewModels;
+using CarouselViewTest.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var peoplePage = new PeoplePage
+            {
+                BindingContext = new PeopleViewModel()
+            };
+
+            MainPage = peoplePage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CarouselViewTest/CarouselViewTest/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CarouselViewTest.Annotations;
+using CarouselViewTest.Models;
+using Xamarin.Forms;
+
+namespace CarouselViewTest.ViewModels
+{
+    internal class PeopleViewModel : INotifyPropertyChanged
+    {
+        private int _position;
+        private ObservableCollection<Person> _people;
+
+        public PeopleViewModel()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person {Age = 23, Name = "Steven"},
+                new Person {Age = 53, Name = "Megan"},
+                new Person {Age = 16, Name = "Joe"},
+                new Person {Age = 67, Name = "Josh"},
+                new Person {Age = 33, Name = "Jessica"},
+                new Person {Age = 29, Name = "Jack"}
+            };
+        }
+
+        public ObservableCollection<Person> People
+        {
+            get => _people;
+            set
+            {
+                if (Equals(value, _people)) return;
+                _people = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(GoNextCommand));
+            }
+        }
+
+        public int Position
+        {
+            get => _position;
+            set
+            {
+                if (value == _position) return;
+                _position = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(GoNextCommand));
+            }
+        }
+
+        public Command GoNextCommand => new Command(() =>
+        {
+            if (People.Count - 1 != Position)
+                Position++;
+        });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
