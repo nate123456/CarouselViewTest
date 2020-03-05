603d42ed3e41f2ede1d11328fe6eaaf1784ae03c
Nathan Whittle <nathan.whittle@colheli.com>
2020-03-05 10:52:17 -0800

    Add Some Coloring to Demonstrate Rotation

---
+++ CarouselViewTest/CarouselViewTest/Converters/AgeToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CarouselViewTest.Converters
+{
+    public class AgeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int age))
+                return Color.LightGreen;
+
+            if (age < 20)
+                return Color.ForestGreen;
+            if (age < 40)
+                return Color.DarkOliveGreen;
+            if (age < 60)
+                return Color.BurlyWood;
+
+            return Color.DarkOliveGreen;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
